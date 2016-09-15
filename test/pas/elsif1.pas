c426487beec4064c417d5b4380f145db1dc4c0f1
Cristian Weiland <cw14@inf.ufpr.br>
2016-09-15 11:56:56 -0300

    Else funciona
    Signed-off-by: Cristian Weiland <cw14@inf.ufpr.br>

--- test/pas/elsif1.pas
+++ test/pas/elsif1.pas
@@ -2,12 +2,12 @@ program p(input, output);
 var a: integer;
 begin
     a := 100;
-    if(a = 10)
+    if(a = 10) then
         a := 1
-    else if(a = 20)
+    else if(a = 20) then
         a := 2
-    else if(a >= 30)
+    else if(a >= 30) then
         a := 3
     else
-        a := 0;
+        a := 0
 end.
