3918d5b8cf794dfc0ed807ee9623e697f8aaea84
Cristian Weiland <cw14@inf.ufpr.br>
2016-09-29 14:30:13 -0300

    Tira chamada de procedimento do teste, mas teste 2 funciona
    Signed-off-by: Cristian Weiland <cw14@inf.ufpr.br>

--- test/pas/procedure2.pas
+++ test/pas/procedure2.pas
@@ -1,12 +1,11 @@
-(* Procedure com 2 params por valor sem sobrescrita em variavel e chamada do procedimento *)
+(* Procedure com 2 params por valor sem sobrescrita em variavel mas sem chamada do procedimento *)
 program p(input, output);
 var a, b: integer;
-procedure p(c: integer, d: integer);
+procedure p(c: integer; d: integer);
     begin
         c := 2
     end;
 begin
     a := 2;
-    b := 1;
-    p(a,b)
+    b := 1
 end.
